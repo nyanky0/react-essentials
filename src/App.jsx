9961d7f96e0f4edde4095fe6ee7de045d3664866
nyanky0 <hwngkyo@gmail.com>
2024-04-22 15:17:32 +0700

    component project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentImg from './assets/components.png';
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title}/>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  const desc = reactDescriptions[genRandomInt(2)];
-  return (
-
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {desc} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
 
 function App() {
   return (

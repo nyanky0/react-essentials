c0ff49f1195973fdc55abff8a5557d27404acdbf
nyanky0 <hwngkyo@gmail.com>
2024-04-25 16:56:04 +0700

    forward props to wrapped elements

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -6,11 +6,11 @@
 //     );
 // }
 
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
 
     return (
         <li>
-            <button className={isSelected ? 'active' : null} onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : null} {...props}>{children}</button>
         </li>
     );
 }
